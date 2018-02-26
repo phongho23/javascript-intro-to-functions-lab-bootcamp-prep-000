5d13789ea9a4f799922f6d0f4adba7c42baab586
Phong Ho <ho.phongt@gmail.com>
2018-02-26 04:27:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma (string) {
   var loveYou = "I love you, too."
   if (string.toLowerCase(string) === string) return cantHear;
   else if (string.toUpperCase(string) === string) return yesIndeed;
-  else (loveGrandma === (string)) return loveYou;
+  else if (string === loveGrandma) return loveYou;
   }
 
 
