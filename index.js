5d8cbee66558cb58243900b905b15d23d838d83c
Phong Ho <ho.phongt@gmail.com>
2018-02-26 04:12:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function sayHiToGrandma (string) {
   var cantHear = "I can't hear you!";
   var yesIndeed = "YES INDEED!"
   var loveGrandma = "I love you, too."
+  var loveGrandma1 = "I love you, Grandma."
   if (string.toLowerCase(string) === string) return cantHear;
   else if (string.toUpperCase(string) === string) return yesIndeed;
-  else (string === "I love you, Grandma.") return loveGrandma;
+  else (string === loveGrandma1) return loveGrandma;
 }
