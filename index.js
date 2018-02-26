e6b608932ebb9b89fcba466b6f0319778e944e3a
Phong Ho <ho.phongt@gmail.com>
2018-02-26 04:14:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,24 +2,3 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-function whisper(string) {
-  return string.toLowerCase()
-}
-
-function logShout (string) {
-  console.log(string.toUpperCase());
-}
- 
- function logWhisper (string) {
-  console.log(string.toLowerCase());
-}
-
-function sayHiToGrandma (string) {
-  var cantHear = "I can't hear you!";
-  var yesIndeed = "YES INDEED!"
-  var loveGrandma = "I love you, too."
-  var loveGrandma1 = "I love you, Grandma."
-  if (string.toLowerCase(string) === string) return cantHear;
-  else if (string.toUpperCase(string) === string) return yesIndeed;
-  else (string === loveGrandma1) return loveGrandma;
-}
